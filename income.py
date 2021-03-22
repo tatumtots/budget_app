0e6930da462d4421c63dfd61e916e1c450d0e53b
Tatum Bray <tatumrbray@gmail.com>
2021-03-22 12:22:42 -0600

    added functions to add function to list and return total

--- income.py
+++ income.py
@@ -3,3 +3,14 @@ def monthly_income(income_list, income):
     income_list.append(income)
     return income_list
 
+
+def get_income_total(income_list):
+    total = 0
+    for inc in income_list:
+        total += int(inc)
+    return total
+
+
+def print_income(income_list):
+    for inc in income_list:
+        print(inc)
